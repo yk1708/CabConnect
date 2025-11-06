e092b8df6b23589dcb7a4fe9d2fe85db26124f76
yash khude <khudeyash2021@gmail.com>
2025-11-06 12:08:59 +0530

    Add Notification File

---
+++ screens/notification/notification.tsx
@@ -0,0 +1,13 @@
+//notification screen
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const NotificationScreen = () => {
+  return (
+    <View>
+      <Text>Notification Screen</Text>
+    </View>
+  )
+}
+
+export default NotificationScreen
